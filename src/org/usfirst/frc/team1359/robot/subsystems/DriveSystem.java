3c0ef0795ed530518b42c27b0d88bbaa9787a50b
tim.helland <tim.helland@LAPTOP-J6VPTH55>
2018-01-13 11:42:53 -0800

    Replace changes lost during reinstall of Eclipse on team laptop.

--- src/org/usfirst/frc/team1359/robot/subsystems/DriveSystem.java
+++ src/org/usfirst/frc/team1359/robot/subsystems/DriveSystem.java
@@ -1,6 +1,11 @@
 package org.usfirst.frc.team1359.robot.subsystems;
 
+import org.usfirst.frc.team1359.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
  *
@@ -9,10 +14,23 @@ public class DriveSystem extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	Talon m_frontLeft = new Talon(RobotMap.frontleftMotor);
+	Talon m_rearLeft = new Talon(RobotMap.rearLeftMotor);
+    SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
+
+    Talon m_frontRight = new Talon(RobotMap.frontRightMotor);
+    Talon m_rearRight = new Talon(RobotMap.rearRightMotor);
+    SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
+
+    DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void tankDrive(double leftSpeed, double rightSpeed) {
+    	
     }
 }
 

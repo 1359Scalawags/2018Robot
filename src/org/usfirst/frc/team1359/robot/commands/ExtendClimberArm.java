111cdc47c6c244d751c701e0b65f08a0c32207e1
tim.helland <tim.helland@lebanon.k12.or.us>
2018-02-07 18:52:04 -0800

    Discussed the purpose of interrupt() with Ryan and pushed an example.

--- src/org/usfirst/frc/team1359/robot/commands/ExtendClimberArm.java
+++ src/org/usfirst/frc/team1359/robot/commands/ExtendClimberArm.java
@@ -22,25 +22,23 @@ public class ExtendClimberArm extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    
     	Robot.kClimber.extendArm();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        
     	return Robot.kClimber.isElevated();    	   
     }
 
 
     // Called once after isFinished returns true
     protected void end() {
-    	
-    	Robot.kClimber.stopArm();
+    	Robot.kClimber.stopElevator();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.kClimber.stopElevator();
     }
 }

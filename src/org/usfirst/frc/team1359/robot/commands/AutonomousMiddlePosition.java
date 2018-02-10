aaa5522f108213a32c78152c85157bf4da14ba58
Ryan <roberts2561@gmail.com>
2018-02-10 14:41:44 -0800

    fixed Desitn's Formatting!

--- src/org/usfirst/frc/team1359/robot/commands/AutonomousMiddlePosition.java
+++ src/org/usfirst/frc/team1359/robot/commands/AutonomousMiddlePosition.java
@@ -11,60 +11,60 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonomousMiddlePosition extends CommandGroup {
 
 	private char switchPos;
-	
-    public AutonomousMiddlePosition() {
-    	super("AutonomousMiddlePositionCommandGroup");
-    	requires(Robot.kPIDDriveSystem);
-    	requires(Robot.kCubeLoader);
-    	DriverStation driverStation = DriverStation.getInstance();
-    	
-    	switchPos = driverStation.getGameSpecificMessage().charAt(0);
-    	
-    	if(switchPos == 'L') {
-    		addSequential(new CubeGrab());
-    		addSequential(new MoveForward(10));
-    		addSequential(new TurnByAngle(-45));
-    		addSequential(new MoveForward(10));
-    		addSequential(new TurnByAngle(45));
-    		addSequential(new MoveForward(10));
-    		if(Robot.MiddlePriority == "Yes") {
-    		addSequential(new CubeAtMiddle());
-    		addSequential(new CubeRelease());	
-    		}else {
-    			
-    		}
-    		
-    	}else {
-    		addSequential(new CubeGrab());
-    		addSequential(new MoveForward(10));
-    		addSequential(new TurnByAngle(45));
-    		addSequential(new MoveForward(10));
-    		addSequential(new TurnByAngle(-45));
-    		addSequential(new MoveForward(10));
-    		if(Robot.MiddlePriority == "Yes") {
-    		addSequential(new CubeAtMiddle());
-    		addSequential(new CubeRelease());	
-    		}else {
-    			
-    		}
-    		
-    	}
-    	
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
 
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
+	public AutonomousMiddlePosition() {
+		super("AutonomousMiddlePositionCommandGroup");
+		requires(Robot.kPIDDriveSystem);
+		requires(Robot.kCubeLoader);
+		DriverStation driverStation = DriverStation.getInstance();
 
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
-    }
+		switchPos = driverStation.getGameSpecificMessage().charAt(0);
+
+		if (switchPos == 'L') {
+			addSequential(new CubeGrab());
+			addSequential(new MoveForward(10));
+			addSequential(new TurnByAngle(-45));
+			addSequential(new MoveForward(10));
+			addSequential(new TurnByAngle(45));
+			addSequential(new MoveForward(10));
+			if (Robot.MiddlePriority == "Yes") {
+				addSequential(new CubeAtMiddle());
+				addSequential(new CubeRelease());
+			} else {
+
+			}
+
+		} else {
+			addSequential(new CubeGrab());
+			addSequential(new MoveForward(10));
+			addSequential(new TurnByAngle(45));
+			addSequential(new MoveForward(10));
+			addSequential(new TurnByAngle(-45));
+			addSequential(new MoveForward(10));
+			if (Robot.MiddlePriority == "Yes") {
+				addSequential(new CubeAtMiddle());
+				addSequential(new CubeRelease());
+			} else {
+
+			}
+
+		}
+
+		// Add Commands here:
+		// e.g. addSequential(new Command1());
+		// addSequential(new Command2());
+		// these will run in order.
+
+		// To run multiple commands at the same time,
+		// use addParallel()
+		// e.g. addParallel(new Command1());
+		// addSequential(new Command2());
+		// Command1 and Command2 will run in parallel.
+
+		// A command group will require all of the subsystems that each member
+		// would require.
+		// e.g. if Command1 requires chassis, and Command2 requires arm,
+		// a CommandGroup containing them would require both the chassis and the
+		// arm.
+	}
 }

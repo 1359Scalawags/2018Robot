9cd3e9179cab7c804ade56972dc59ea785edb6f7
Ryan <roberts2561@gmail.com>
2018-02-18 13:45:52 -0800

    changed talon ports in Robot class

--- src/org/usfirst/frc/team1359/robot/Robot.java
+++ src/org/usfirst/frc/team1359/robot/Robot.java
@@ -89,7 +89,7 @@ public class Robot extends TimedRobot {
 		System.out.println("The 1359 Scalawags can win this match!  ");
 		CameraServer.getInstance().startAutomaticCapture();
 		driverStation = DriverStation.getInstance();
-		LiveWindow.add(new Talon(0));
+		LiveWindow.add(new Talon(1));
 		LiveWindow.add(new Talon(2));
        
 	}
